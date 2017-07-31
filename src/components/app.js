32ba07615f6fbe0e2d5c19ecc4fbace1c37b52d8
Ashley Deane <ashley.deane@openet.com>
2017-07-31 17:09:00 +0100

    Start Redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+//Containers
+import BookList from '../containers/book_list'
+
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
